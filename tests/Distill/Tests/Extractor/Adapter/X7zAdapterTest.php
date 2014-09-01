759d5800edf80917741bd5151718f0de35e93ada
Raul Fraile <raulfraile@gmail.com>
2014-09-01 23:38:11 +0200

    [test] Fake/corrupt xz files

--- tests/Distill/Tests/Extractor/Adapter/X7zAdapterTest.php
+++ tests/Distill/Tests/Extractor/Adapter/X7zAdapterTest.php
@@ -9,7 +9,7 @@ use Distill\Format\X7z;
 class X7zAdapterTest extends AbstractAdapterTest
 {
 
-    public function testExtractCorrectRarFileWith7zCommand()
+    public function testExtractCorrect7ZFileWith7zCommand()
     {
         $target = $this->getTemporaryPath();
         $this->clearTemporaryPath();
@@ -22,6 +22,21 @@ class X7zAdapterTest extends AbstractAdapterTest
         $this->assertTrue($response);
 
         $this->checkDirectoryFiles($target, $this->filesPath . '/uncompressed');
+        $this->clearTemporaryPath();
+    }
+
+    public function testExtractFake7ZFileWith7zCommand()
+    {
+        $target = $this->getTemporaryPath();
+        $this->clearTemporaryPath();
+
+        $this->adapter = new X7zAdapter(array(
+            array('self', 'extract7zCommand')
+        ));
+
+        $response = $this->adapter->extract(new File($this->filesPath . 'file_fake.7z', new X7z()), $target);
+        $this->assertFalse($response);
+
         $this->clearTemporaryPath();
     }
 
