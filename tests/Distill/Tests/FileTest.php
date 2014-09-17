cd3a564f8aa8dd7fb1c4e10cb78c3426de41a1af
Raul Fraile <raulfraile@gmail.com>
2014-09-17 11:01:07 +0200

    Test File class

---
+++ tests/Distill/Tests/FileTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Distill\Tests\Format;
+
+use Distill\File;
+use Distill\Format;
+
+class FileTest extends \PHPUnit_Framework_TestCase
+{
+
+
+    public function testConstructorParameters()
+    {
+        $file = new File('test.zip', new Format\Zip());
+
+        $this->assertEquals('test.zip', $file->getPath());
+        $this->assertInstanceOf('\\Distill\\Format\\Zip', $file->getFormat());
+    }
+
+    public function testSetters()
+    {
+        $file = new File('test.zip', new Format\Zip());
+
+        $file->setPath('test.tgz');
+        $file->setFormat(new Format\TarGz());
+
+        $this->assertEquals('test.tgz', $file->getPath());
+        $this->assertInstanceOf('\\Distill\\Format\\TarGz', $file->getFormat());
+    }
+
+    public function testToStringReturnsPath()
+    {
+        $file = new File('test.zip', new Format\Zip());
+
+        $this->assertEquals('test.zip', (string) $file);
+    }
+
+}
