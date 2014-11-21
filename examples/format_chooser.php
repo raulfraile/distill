0d8b3fb74e493ef330d30fcdc1e9a87b4a11fc1f
Raul Fraile <raulfraile@gmail.com>
2014-11-21 12:52:42 +0100

    Update format chooser example

--- examples/format_chooser.php
+++ examples/format_chooser.php
@@ -3,14 +3,24 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Distill\Distill;
+use Distill\Strategy\MinimumSize;
 
 $distill = new Distill();
+$strategy = new MinimumSize();
 
 $preferredFile = $distill
     ->getChooser()
-    ->setStrategy(new \Distill\Strategy\UncompressionSpeed())
+    ->setStrategy($strategy)
     ->addFile('http://get.symfony.com/Symfony_Standard_Vendors_2.5.3.zip')
     ->addFile('http://get.symfony.com/Symfony_Standard_Vendors_2.5.3.tgz')
+    ->getPreferredFile();
+
+echo $preferredFile . \PHP_EOL;
+
+$preferredFile = $distill
+    ->getChooser()
+    ->setStrategy($strategy)
+    ->addFilesWithDifferentExtensions('http://get.symfony.com/Symfony_Standard_Vendors_2.5.3', ['zip', 'tgz'])
     ->getPreferredFile();
 
 echo $preferredFile . \PHP_EOL;
